5f000689d4e7f1fcf11f420b766b50d5f17397f7
Hector <hectojo_2109@hotmail.com>
2019-10-14 11:55:49 -0400

    Agregar archivos de proyecto.

---
+++ Votowf/Contenedor.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Votowf
+{
+    partial class Contenedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Contenedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.IsMdiContainer = true;
+            this.Name = "Contenedor";
+            this.ShowIcon = false;
+            this.Text = "Contenedor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
