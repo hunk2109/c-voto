c5c2ab8d459d91823e874470bf94d3fc859e0d1d
Hector <hectojo_2109@hotmail.com>
2019-10-16 10:38:52 -0400

    11

--- Votowf/Form1.cs
+++ Votowf/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.QrCode;
 
 namespace Votowf
 {
@@ -17,6 +21,13 @@ namespace Votowf
             InitializeComponent();
         }
 
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Image)eventArgs.Frame.Clone();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Contenedor f = new Contenedor();
@@ -37,6 +48,66 @@ namespace Votowf
             this.ControlBox = false;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalFrame.IsRunning == true)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button2.Enabled = false;
+            textBox1.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BarcodeReader Reader = new BarcodeReader();
+            Result result = Reader.Decode((Bitmap)pictureBox1.Image);
+            try
+            {
+                string decoded = result.ToString().Trim();
+                textBox1.Text = decoded;
+
+                if (decoded == "12345")
+                {
+                    timer1.Stop();
+                    button2.Enabled = true;
+                    textBox1.Text = decoded;
+                    partidos f = new partidos();
+                    f.MdiParent = this.MdiParent;
+                    f.Show();
+                    this.Close();
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
