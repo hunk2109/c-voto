dccae116cf1fac1aa86df75958e3dac0fd096e3a
Hector <hectojo_2109@hotmail.com>
2019-10-25 11:22:23 -0400

    54

--- Votowf/partidos.Designer.cs
+++ Votowf/partidos.Designer.cs
@@ -59,7 +59,7 @@
             this.label2.Location = new System.Drawing.Point(59, 494);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 7;
             this.label2.Text = "No votar";
             // 
             // label3
